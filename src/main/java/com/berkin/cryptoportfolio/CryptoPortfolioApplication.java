4930d0b8cd546f06e8f36754fceef0444a1c53b4
Berkin Tosun <berkintsn@gmail.com>
2024-02-27 23:47:43 +0200

    Adding RestTemplate Bean

--- src/main/java/com/berkin/cryptoportfolio/CryptoPortfolioApplication.java
+++ src/main/java/com/berkin/cryptoportfolio/CryptoPortfolioApplication.java
@@ -2,6 +2,9 @@ package com.berkin.cryptoportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CryptoPortfolioApplication {
@@ -10,4 +13,8 @@ public class CryptoPortfolioApplication {
 		SpringApplication.run(CryptoPortfolioApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
